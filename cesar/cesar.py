b86c16c303a1d593aef444d6f2f2ceb305224299
Silvério Simões <silverio.simoes@gmail.com>
2022-07-29 23:19:01 -0300

    Deletando ponto final da docstring para teste de hooks de pre-commit

--- cesar/cesar.py
+++ cesar/cesar.py
@@ -2,7 +2,7 @@ from string import ascii_lowercase
 
 
 def encripta(frase, rot=13):
-    """Encripta o texto"""
+    """Encripta o texto."""
     encriptado = ''
     for char in frase:
         char = char.lower()
@@ -18,7 +18,7 @@ def encripta(frase, rot=13):
 
 
 def decripta(frase, rot=13):
-    """Decripta o texto"""
+    """Decripta o texto."""
     decriptado = ''
     for char in frase:
         char = char.lower()
